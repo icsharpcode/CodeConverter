f9da9eb47cb0cef79dc410f9c51d539035ea18da
GrahamTheCoder <grahamthecoder@gmail.com>
2018-04-22 19:29:52 +0100

    Declare type for func

--- Tests/CSharp/ExpressionTests.cs
+++ Tests/CSharp/ExpressionTests.cs
@@ -445,14 +445,16 @@ class TestClass : BaseTestClass
         {
             TestConversionVisualBasicToCSharp(@"Class TestClass
     Private Sub TestMethod()
-        Dim test = Function(ByVal a As Integer) a * 2
+        Dim test As Func(Of Integer, Integer) = Function(ByVal a As Integer) a * 2
         test(3)
     End Sub
-End Class", @"class TestClass
+End Class", @"using System;
+
+class TestClass
 {
     private void TestMethod()
     {
-        var test = int a => a * 2;
+        Func<int, int> test = int a => a * 2;
         test(3);
     }
 }");
