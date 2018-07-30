57f02c95f25c1d145ca78a95d90dddcc7826d5b1
GrahamTheCoder <grahamthecoder@gmail.com>
2018-07-30 23:36:16 +0100

    Use compiler's support for this structure for commonly used methods
    Maybe it'll be faster, should certainly be easier to inline if we decide the methods are now irrelevant (though they're still slightly shorter than the block syntax, they're quite tricky to write because of the complicated type signature)

--- ICSharpCode.CodeConverter/Util/ObjectExtensions.cs
+++ ICSharpCode.CodeConverter/Util/ObjectExtensions.cs
@@ -1929,12 +1929,13 @@ namespace ICSharpCode.CodeConverter.Util
 
         public static TResult TypeSwitch<TBaseType, TDerivedType1, TResult>(this TBaseType obj, Func<TDerivedType1, TResult> matchFunc1, Func<TBaseType, TResult> defaultFunc = null) where TDerivedType1 : TBaseType
         {
-            if (obj is TDerivedType1) {
-                return matchFunc1((TDerivedType1)obj);
-            } else if (defaultFunc != null) {
-                return defaultFunc(obj);
-            } else {
-                return default(TResult);
+            switch (obj) {
+                case TDerivedType1 tObj:
+                    return matchFunc1(tObj);
+                case TBaseType tObj when (defaultFunc != null):
+                    return defaultFunc(tObj);
+                default:
+                    return default(TResult);
             }
         }
 
@@ -1942,14 +1943,15 @@ namespace ICSharpCode.CodeConverter.Util
             where TDerivedType1 : TBaseType
             where TDerivedType2 : TBaseType
         {
-            if (obj is TDerivedType1) {
-                return matchFunc1((TDerivedType1)obj);
-            } else if (obj is TDerivedType2) {
-                return matchFunc2((TDerivedType2)obj);
-            } else if (defaultFunc != null) {
-                return defaultFunc(obj);
-            } else {
-                return default(TResult);
+            switch (obj) {
+                case TDerivedType1 tObj:
+                    return matchFunc1(tObj);
+                case TDerivedType2 tObj:
+                    return matchFunc2(tObj);
+                case TBaseType tObj when (defaultFunc != null):
+                    return defaultFunc(tObj);
+                default:
+                    return default(TResult);
             }
         }
 
@@ -1958,16 +1960,17 @@ namespace ICSharpCode.CodeConverter.Util
             where TDerivedType2 : TBaseType
             where TDerivedType3 : TBaseType
         {
-            if (obj is TDerivedType1) {
-                return matchFunc1((TDerivedType1)obj);
-            } else if (obj is TDerivedType2) {
-                return matchFunc2((TDerivedType2)obj);
-            } else if (obj is TDerivedType3) {
-                return matchFunc3((TDerivedType3)obj);
-            } else if (defaultFunc != null) {
-                return defaultFunc(obj);
-            } else {
-                return default(TResult);
+            switch (obj) {
+                case TDerivedType1 tObj:
+                    return matchFunc1(tObj);
+                case TDerivedType2 tObj:
+                    return matchFunc2(tObj);
+                case TDerivedType3 tObj:
+                    return matchFunc3(tObj);
+                case TBaseType tObj when (defaultFunc != null):
+                    return defaultFunc(tObj);
+                default:
+                    return default(TResult);
             }
         }
 
