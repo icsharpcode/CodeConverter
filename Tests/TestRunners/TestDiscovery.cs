9f6d8de65b5b14bd5f7a1aa8114cada292245e65
GrahamTheCoder <grahamthecoder@gmail.com>
2018-12-02 18:33:41 +0000

    Slightly dead end trying to use bits of xunit to easily discover/run tests - will try nunit instead

--- Tests/TestRunners/TestDiscovery.cs
+++ Tests/TestRunners/TestDiscovery.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace CodeConverter.Tests.TestRunners
 {
     /// <summary>
     /// Discover and return xUnit tests in given assemblies.
     /// </summary>
-    public static class TestDiscovery
+    public class TestDiscovery : XunitTestFramework//XunitTestFrameworkDiscoverer
     {
         public static Dictionary<string, Action> GetTestNamesAndCallbacks(byte[] compiledIL)
         {
@@ -19,6 +21,8 @@ namespace CodeConverter.Tests.TestRunners
 
         public static Dictionary<string, Action> GetTestNamesAndCallbacks(Assembly assembly)
         {
+            List<object> obj = new List<object>();
+            new TestDiscovery(new LambdaMessageSink(m => obj.Add(m))).GetDiscoverer(new ReflectionAssemblyInfo(assembly)).Find(false, new LambdaMessageSink(m => obj.Add(m)), new TestDiscoveryOptions());
             var factMethods = DiscoverFactMethods(assembly);
             return factMethods.ToDictionary(GetFullName, m => new Action(() => {
                 var instance = Activator.CreateInstance(m.DeclaringType);
@@ -34,5 +38,42 @@ namespace CodeConverter.Tests.TestRunners
         }
 
         private static string GetFullName(MethodInfo method) => method.DeclaringType.FullName + "." + method.Name;
+        
+
+        private class LambdaMessageSink : IMessageSink
+        {
+            private readonly Action<IMessageSinkMessage> _action;
+
+            public LambdaMessageSink(Action<IMessageSinkMessage> action)
+            {
+                _action = action;
+            }
+
+            public bool OnMessage(IMessageSinkMessage message)
+            {
+                //if (message is DiscoveryCompleteMessage)
+                _action(message);
+                return true;
+            }
+        }
+
+        private class TestDiscoveryOptions : ITestFrameworkDiscoveryOptions
+        {
+            private readonly Dictionary<string, object> _options = new Dictionary<string, object>();
+
+            public TValue GetValue<TValue>(string name)
+            {
+                return  _options.TryGetValue(name, out var option) ? (TValue)option : default(TValue);
+            }
+
+            public void SetValue<TValue>(string name, TValue value)
+            {
+                _options[name] = value;
+            }
+        }
+
+        public TestDiscovery(IMessageSink messageSink) : base(messageSink)
+        {
+        }
     }
 }
