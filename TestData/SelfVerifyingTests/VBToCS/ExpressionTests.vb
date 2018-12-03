99b794af065957a84928c4e986b49682752c5922
taylus <brandon@legacy.codes>
2018-12-02 18:40:29 -0600

    Uncomment decimal division test case after rebasing to pick up #216

--- TestData/SelfVerifyingTests/VBToCS/ExpressionTests.vb
+++ TestData/SelfVerifyingTests/VBToCS/ExpressionTests.vb
@@ -25,14 +25,11 @@ Module Program
             Assert.True(areEqual)
         End Sub
 
-        ' Message: Error compiling target: CodeConverter.Tests.Compilation.CompilationException: Compilation failed:
-        ' (29,21) Error CS0019 : Operator '/' cannot be applied to operands of type 'decimal' and 'double'
-        ' https://github.com/icsharpcode/CodeConverter/issues/202
-        '<Fact>
-        'Public Sub TestDecimalDivision()
-        '   Dim x = 7D / 2D
-        '   Assert.Equal(x, 3.5D)
-        'End Sub
+        <Fact>
+        Public Sub TestDecimalDivision()
+            Dim x = 7D / 2D
+            Assert.Equal(x, 3.5D)
+        End Sub
 
     End Class
 
