2a55e9b9c0a5b40a6c735f0b5867edb586f1e03f
GrahamTheCoder <grahamthecoder@gmail.com>
2019-01-16 00:51:12 +0000

    Remove unused

--- CodeConverter.Web/Program.cs
+++ CodeConverter.Web/Program.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace CodeConverter.Web
 {
