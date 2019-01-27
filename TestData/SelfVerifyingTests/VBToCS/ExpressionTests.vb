2c056d7918373897235a6c2df41c77007df472ad
GrahamTheCoder <grahamthecoder@gmail.com>
2019-01-27 12:58:52 +0000

    Test that current conversion has the correct behaviour on decimals
    Guards against breaking currently correct behaviour regarding #238

--- TestData/SelfVerifyingTests/VBToCS/ExpressionTests.vb
+++ TestData/SelfVerifyingTests/VBToCS/ExpressionTests.vb
@@ -24,6 +24,22 @@ Module Program
             Assert.Equal(x, 3.5D)
         End Sub
 
+        <Fact>
+        Public Sub TestIntFunctionFloorsDecimal() 'https://github.com/icsharpcode/CodeConverter/issues/238
+            Dim a = 30.4
+            Dim b = 20.5
+            Dim c = 10.6
+            Dim d = -10.4
+            Dim e = -20.5
+            Dim f = -30.6
+            Assert.Equal(30, Int(a))
+            Assert.Equal(20, Int(b))
+            Assert.Equal(10, Int(c))
+            Assert.Equal(-11, Int(d))
+            Assert.Equal(-21, Int(e))
+            Assert.Equal(-31, Int(f))
+        End Sub
+
         <Fact(Skip := "https://github.com/icsharpcode/CodeConverter/issues/105")>
         Public Sub EmptyStringEqualityToNull()
             Dim s As String = ""
