a473e45307f3567693b1e17a95893cb4d85d1b57
GrahamTheCoder <grahamthecoder@gmail.com>
2019-08-17 13:59:06 +0100

    Method to expand all nodes

--- ICSharpCode.CodeConverter/CSharp/DocumentExtensions.cs
+++ ICSharpCode.CodeConverter/CSharp/DocumentExtensions.cs
@@ -23,5 +23,45 @@ namespace ICSharpCode.CodeConverter.CSharp
             var withSyntaxRoot = doc.WithSyntaxRoot(root.WithAdditionalAnnotations(Simplifier.Annotation));
             return await Simplifier.ReduceAsync(withSyntaxRoot);
         }
+
+        public static async Task<Document> WithExpandedSyntaxRootAsync(this Document doc)
+        {
+            var syntaxRootAsync = await doc.GetSyntaxRootAsync();
+            var expandableNodes = syntaxRootAsync.DescendantNodes(n => !CanBeExpanded(n)).Where(CanBeExpanded).ToList();
+            syntaxRootAsync = await syntaxRootAsync.ReplaceNodesAsync(expandableNodes,
+                (original, _, token) => Simplifier.ExpandAsync(original, doc, cancellationToken: token), CancellationToken.None);
+            return doc.WithSyntaxRoot(syntaxRootAsync);
+        }
+
+        private static bool CanBeExpanded(SyntaxNode node)
+        {
+            return CanVbNodeBeExpanded(node) || CanCsNodeBeExpanded(node);
+        }
+
+        private static bool CanCsNodeBeExpanded(SyntaxNode node)
+        {
+            return node is CSharpSyntaxNode &&
+                   (node is CSSyntax.AttributeSyntax ||
+                    node is CSSyntax.AttributeArgumentSyntax ||
+                    node is CSSyntax.ConstructorInitializerSyntax ||
+                    node is CSSyntax.ExpressionSyntax ||
+                    node is CSSyntax.FieldDeclarationSyntax ||
+                    node is CSSyntax.StatementSyntax ||
+                    node is CSSyntax.CrefSyntax ||
+                    node is CSSyntax.XmlNameAttributeSyntax ||
+                    node is CSSyntax.TypeConstraintSyntax ||
+                    node is CSSyntax.BaseTypeSyntax);
+        }
+
+        private static bool CanVbNodeBeExpanded(SyntaxNode vbNode)
+        {
+            return vbNode is VBasic.VisualBasicSyntaxNode &&
+                   (vbNode is VBSyntax.ExpressionSyntax ||
+                    vbNode is VBSyntax.StatementSyntax ||
+                    vbNode is VBSyntax.AttributeSyntax ||
+                    vbNode is VBSyntax.SimpleArgumentSyntax ||
+                    vbNode is VBSyntax.CrefReferenceSyntax ||
+                    vbNode is VBSyntax.TypeConstraintSyntax);
+        }
     }
 }
