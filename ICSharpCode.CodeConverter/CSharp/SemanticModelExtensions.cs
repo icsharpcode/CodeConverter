027df23f35fcb1e3b8ef3a103389dd240bd4a7de
GrahamTheCoder <grahamthecoder@gmail.com>
2019-08-31 16:08:45 +0100

    API says data flows in even from unintialized vars => reflection time

--- ICSharpCode.CodeConverter/CSharp/SemanticModelExtensions.cs
+++ ICSharpCode.CodeConverter/CSharp/SemanticModelExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using ICSharpCode.CodeConverter.Util;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.FindSymbols;
@@ -14,32 +17,41 @@ namespace ICSharpCode.CodeConverter.CSharp
 {
     internal static class SemanticModelExtensions
     {
+        private static PropertyInfo _unassignedVariablesProperty;
+
         /// <summary>
         /// This check is entirely to avoid some unnecessary default initializations so the code looks less cluttered and more like the VB did.
         /// The caller should default to outputting an initializer which is always safe for equivalence/correctness.
         /// </summary>
-        /// <remarks>Unfortunately the roslyn UnassignedVariablesWalker is internal only
-        /// https://github.com/dotnet/roslyn/blob/007022c37c6d21ee100728954bd75113e0dfe4bd/src/Compilers/VisualBasic/Portable/Analysis/FlowAnalysis/UnassignedVariablesWalker.vb#L15
-        /// It'd be possible to see the result of the diagnostic analysis, but that would miss out value types, which don't cause a warning in VB
-        /// </remarks>
         public static bool IsDefinitelyAssignedBeforeRead(this SemanticModel semanticModel, Document document,
             ISymbol localSymbol, ModifiedIdentifierSyntax name)
         {
             MethodBlockBaseSyntax methodBlockBaseSyntax = name.GetAncestor<MethodBlockBaseSyntax>();
             var methodFlow = semanticModel.AnalyzeDataFlow(methodBlockBaseSyntax.Statements.First(), methodBlockBaseSyntax.Statements.Last());
             if (!methodFlow.ReadInside.Contains(localSymbol)) return true;
-            if (!methodFlow.AlwaysAssigned.Contains(localSymbol)) return false;
-            var nameStmt = name.GetAncestor<StatementSyntax>();
+            var unassignedVariables = GetUnassignedVariables(methodFlow);
+            if (unassignedVariables != null) return !unassignedVariables.Contains(localSymbol);
+            //Safe underapproximation - may not be read on the paths where it wasn't assigned
+            return methodFlow.AlwaysAssigned.Contains(localSymbol);
+        }
 
-            var references = SymbolFinder.FindReferencesAsync(localSymbol, document.Project.Solution).GetAwaiter().GetResult().ToList();//TODO asyncify
-            return references.SelectMany(r => r.Locations)
-                .Select(r => AnalyzeDataFlow(semanticModel, methodBlockBaseSyntax, r))
-                .All(nodeFlow => !nodeFlow.ReadInside.Contains(localSymbol) || nodeFlow.DataFlowsIn.Contains(localSymbol));
+        /// <remarks>Unfortunately the roslyn UnassignedVariablesWalker and all useful collections created from it are internal only
+        /// Other attempts using DataFlowsIn on each reference showed that "DataFlowsIn" even from an uninitialized variable (at least in the case of ints)
+        /// https://github.com/dotnet/roslyn/blob/007022c37c6d21ee100728954bd75113e0dfe4bd/src/Compilers/VisualBasic/Portable/Analysis/FlowAnalysis/UnassignedVariablesWalker.vb#L15
+        /// It'd be possible to see the result of the diagnostic analysis, but that would miss out value types, which don't cause a warning in VB
+        /// </remarks>
+        private static IEnumerable<ISymbol> GetUnassignedVariables(DataFlowAnalysis methodFlow)
+        {
+            _unassignedVariablesProperty = _unassignedVariablesProperty ?? methodFlow.GetType().GetProperty("UnassignedVariables");
+            if (_unassignedVariablesProperty != null && _unassignedVariablesProperty.GetValue(methodFlow) is IEnumerable unassignedVariables)
+                return unassignedVariables.Cast<ISymbol>();
+            return null;
         }
 
         private static DataFlowAnalysis AnalyzeDataFlow(SemanticModel semanticModel, SyntaxNode ancestorOfLocation, ReferenceLocation location)
         {
-            return AnalyzeDataFlow(semanticModel, ancestorOfLocation.FindNode(location.Location.SourceSpan));
+            var ofLocation = ancestorOfLocation.FindNode(location.Location.SourceSpan);
+            return AnalyzeDataFlow(semanticModel, ofLocation);
         }
 
         private static DataFlowAnalysis AnalyzeDataFlow(SemanticModel semanticModel, SyntaxNode refNode)
