bd3efacdbe7cf0671e73cfdaa32c19b79eb287c0
Brian Freeman <freeman@carnegie.com>
2019-10-17 17:16:16 -0400

    remove TODO comment

--- ICSharpCode.CodeConverter/CSharp/TypeConversionAnalyzer.cs
+++ ICSharpCode.CodeConverter/CSharp/TypeConversionAnalyzer.cs
@@ -86,9 +86,8 @@ namespace ICSharpCode.CodeConverter.CSharp
         public TypeConversionKind AnalyzeConversion(Microsoft.CodeAnalysis.VisualBasic.Syntax.ExpressionSyntax vbNode, bool alwaysExplicit = false)
         {
             var typeInfo = ModelExtensions.GetTypeInfo(_semanticModel, vbNode);
-            var vbType = typeInfo.Type; 
+            var vbType = typeInfo.Type;
             var vbConvertedType = typeInfo.ConvertedType;
-
             if (vbType is null || vbConvertedType is null)
             {
                 return TypeConversionKind.Unknown;
@@ -181,11 +180,9 @@ namespace ICSharpCode.CodeConverter.CSharp
             if (vbConversion.IsNumeric && (vbType.IsEnumType() || vbConvertedType.IsEnumType())) {
                 return TypeConversionKind.NonDestructiveCast;
             }
-            //TODO: Conversion for implicit object to string conversion handle case vbTYpe="Error ?"
             if (alwaysExplicit) {
                 return vbConversion.IsNarrowing ? TypeConversionKind.NonDestructiveCast : TypeConversionKind.DestructiveCast;
             }
-       
 
             return TypeConversionKind.Unknown;
         }
