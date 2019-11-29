4eedcbe09ec9ac196310f114677c309a075a1c0c
GrahamTheCoder <grahamthecoder@gmail.com>
2019-11-29 11:04:25 +0000

    Don't convert lambdas to local functions if they aren't initializing something close by

---
+++ ICSharpCode.CodeConverter/CSharp/OperationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace ICSharpCode.CodeConverter.CSharp
+{
+    internal static class OperationExtensions
+    {
+        public static IOperation GetParentIgnoringConversions(this IOperation operation)
+        {
+            var parent = operation?.Parent;
+            do {
+                parent = parent?.Parent;
+            } while (parent is IConversionOperation || parent is IParenthesizedOperation);
+
+            return parent;
+        }
+    }
+}
