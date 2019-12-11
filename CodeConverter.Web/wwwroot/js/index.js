57ad21b960051298ed8074b6ec2be7b0a9fca893
Christoph Wille <christoph.wille@gmail.com>
2019-12-11 17:25:32 +0100

    AngularJS 1.5 -> Vue 1.6.10

--- CodeConverter.Web/wwwroot/js/index.js
+++ CodeConverter.Web/wwwroot/js/index.js
@@ -1,77 +1,95 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification for details on configuring this project to bundle and minify static web assets.
+﻿var vbToCsId = "vbnet2cs";
+var defaultVbInput = "Public Class VisualBasicClass\r\n\r\nEnd Class";
+var defaultCsInput = "public class CSharpClass\r\n{\r\n\r\n}";
 
-var app = angular.module('ccApp', []);
-
-app.controller('ccController', function ($scope, $timeout, $http) {
-    const vbToCsId = "vbnet2cs";
-    const defaultVbInput = "Public Class VisualBasicClass\r\n\r\nEnd Class";
-    const defaultCsInput = "public class CSharpClass\r\n{\r\n\r\n}";
-    $scope.convertedCode = "";
-    $scope.requestedConversion = vbToCsId;
-    $scope.errorMessageOnResponse = "";
-    $scope.showErrors = false;
-    $scope.converterCallInflight = false;
-    $scope.inputCodeLanguage = function () { return $scope.requestedConversion === vbToCsId ? "VB.NET" : "C#"; };
-    $scope.outputCodeLanguage = function () { return $scope.requestedConversion === vbToCsId ? "C#" : "VB.NET"; };
-
-    $scope.setDefaultInput = function () {
-
-        if (!$scope.inputCode || $scope.inputCode === defaultVbInput || $scope.inputCode === defaultCsInput) {
-            $scope.inputCode = $scope.requestedConversion === vbToCsId ? defaultVbInput : defaultCsInput;
-            $scope.convertedCode = "";
-        } else if ($scope.convertedCode) {
-            const tempInput = $scope.inputCode;
-            $scope.inputCode = $scope.convertedCode;
-            $scope.convertedCode = tempInput;
+var app = new Vue({
+    el: '#app',
+    data: {
+        inputCode: defaultVbInput,
+        requestedConversion: vbToCsId,
+        convertedCode: "",
+        converterCallInflight: false,
+        errorMessageOnResponse: "",
+        showErrors: false
+    },
+    watch: {
+        requestedConversion: function (newConversion, oldConversion) {
+            this.setDefaultInput();
         }
+    },
+    computed: {
+        inputCodeLanguage: function () {
+            return this.requestedConversion === vbToCsId ? "VB.NET" : "C#";
+        },
+        outputCodeLanguage: function () {
+            return this.requestedConversion === vbToCsId ? "C#" : "VB.NET";
+        }
+    },
+    methods: {
+        convert: function () {
+            this.converterCallInflight = true;
+            this.convertedCode = "";
+            this.errorMessageOnResponse = "";
+            this.showErrors = false;
 
-        $timeout(function() {
-            const input = document.getElementById("inputTextArea");
-            input.focus();
-            input.select();
-        });
-    };
+            var xhttp = new XMLHttpRequest();
+            var capturedObj = this;
 
-    $scope.convert = function () {
-        $scope.converterCallInflight = true;
-        $scope.convertedCode = "";
-        $scope.errorMessageOnResponse = "";
-        $scope.showErrors = false;
-
-        var data = JSON.stringify({code: $scope.inputCode, requestedConversion: $scope.requestedConversion });
-
-        $http.post('/api/converter/', data).then(function successCallback(response) {
-                $scope.converterCallInflight = false;
-                $scope.convertedCode = response.data.convertedCode;
-
-                // Wait for render before selecting output (ready to copy and paste)
-                $timeout(function() {
-                    const output = document.getElementById("outputTextArea");
-                    output.focus();
-                    output.select();
-                });
-
-                if (!response.data.conversionOk) {
-                    $scope.showErrors = true;
-                    $scope.errorMessageOnResponse = response.data.errorMessage;
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    capturedObj.converterCallInflight = false;
                 }
-            },
-            function errorCallback(response) {
-                $scope.converterCallInflight = false;
-                $scope.showErrors = true;
-                $scope.errorMessageOnResponse = "Call to the server backend failed";
-            });
-    };
-    window.onload = function () {
-        // Ctrl-enter converts
-        $(document).bind("keydown", function(e) {
-            if ((e.ctrlKey || e.metaKey) && (e.keyCode === 13 || e.keyCode === 10)) {
-                document.getElementById("convert-button").click();
-                e.preventDefault();
-            }
-        });
-        $scope.setDefaultInput();
-    };
+                if (this.readyState === 4 && this.status === 200) {
+                    var response = JSON.parse(this.responseText);
+                    capturedObj.convertedCode = response.convertedCode;
 
-    $scope.setDefaultInput();
+                    if (!response.conversionOk) {
+                        capturedObj.showErrors = true;
+                        capturedObj.errorMessageOnResponse = response.errorMessage;
+                    }
+
+                    setTimeout(function () {
+                        var output = document.getElementById("outputTextArea");
+                        output.focus();
+                        output.select();
+                    });
+                }
+            };
+            xhttp.open("POST", "/api/converter/", true);
+            xhttp.setRequestHeader("Content-Type", "application/json");
+
+            var data = JSON.stringify({ code: this.inputCode, requestedConversion: this.requestedConversion });
+            xhttp.send(data);
+        },
+        setDefaultInput: function () {
+            if (!this.inputCode || this.inputCode === defaultVbInput || this.inputCode === defaultCsInput) {
+                this.inputCode = this.requestedConversion === vbToCsId ? defaultVbInput : defaultCsInput;
+                this.convertedCode = "";
+            } else if (this.convertedCode) {
+                var tempInput = this.inputCode;
+                this.inputCode = this.convertedCode;
+                this.convertedCode = tempInput;
+            }
+
+            setTimeout(function () {
+                var input = document.getElementById("inputTextArea");
+                input.focus();
+                input.select();
+            });
+        }
+    },
+    beforeMount() {
+        this.setDefaultInput();
+
+        window.onload = function () {
+            // Ctrl-enter converts
+            $(document).bind("keydown", function (e) {
+                if ((e.ctrlKey || e.metaKey) && (e.keyCode === 13 || e.keyCode === 10)) {
+                    document.getElementById("convert-button").click();
+                    e.preventDefault();
+                }
+            });
+            this.setDefaultInput();
+        };
+    }
 });
