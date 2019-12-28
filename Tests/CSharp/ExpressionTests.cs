9d78b40122eb82c138254e4d637a14f4ffc8bfe8
GrahamTheCoder <grahamthecoder@gmail.com>
2019-12-28 19:49:27 +0000

    Use last part of name

--- Tests/CSharp/ExpressionTests.cs
+++ Tests/CSharp/ExpressionTests.cs
@@ -416,13 +416,13 @@ public partial class Class1
     End Function
 End Class", @"public partial class Class1
 {
-    static Class1 global__p1()
+    static Class1 Foo__p1()
     {
         var argc1 = new Class1();
         return Foo(ref argc1);
     }
 
-    private Class1 _p1 = global__p1();
+    private Class1 _p1 = Foo__p1();
 
     public static Class1 Foo(ref Class1 c1)
     {
