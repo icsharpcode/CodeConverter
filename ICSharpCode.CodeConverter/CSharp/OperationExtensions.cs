86b026234f4e68e7d3ac9524ff8fa7da6242e89f
GrahamTheCoder <grahamthecoder@gmail.com>
2019-12-28 19:47:50 +0000

    Digression

--- ICSharpCode.CodeConverter/CSharp/OperationExtensions.cs
+++ ICSharpCode.CodeConverter/CSharp/OperationExtensions.cs
@@ -7,12 +7,28 @@ namespace ICSharpCode.CodeConverter.CSharp
     {
         public static IOperation GetParentIgnoringConversions(this IOperation operation)
         {
-            var parent = operation?.Parent;
+            var parent = operation?.Parent; //TODO This appears to skip up two layers, rename or change logic
             do {
                 parent = parent?.Parent;
             } while (parent is IConversionOperation || parent is IParenthesizedOperation);
 
             return parent;
+        }
+        public static IOperation GetNonConversionOperation(this IOperation operation)
+        {
+            while (true) {
+                switch (operation)
+                {
+                    case IConversionOperation co:
+                        operation = co.Operand;
+                        break;
+                    case IParenthesizedOperation po:
+                        operation = po.Operand;
+                        break;
+                    default:
+                        return operation;
+                }
+            }
         }
     }
 }
