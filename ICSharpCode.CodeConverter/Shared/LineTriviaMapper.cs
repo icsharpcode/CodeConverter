ec8799a40d0eccc5442a0bd27c5fcf567ffa8850
GrahamTheCoder <grahamthecoder@gmail.com>
2020-02-02 10:22:42 +0000

    renames

--- ICSharpCode.CodeConverter/Shared/LineTriviaMapper.cs
+++ ICSharpCode.CodeConverter/Shared/LineTriviaMapper.cs
@@ -9,18 +9,18 @@ namespace ICSharpCode.CodeConverter.Shared
 {
     internal class LineTriviaMapper
     {
-        private readonly SyntaxNode source;
-        private readonly TextLineCollection sourceLines;
-        private readonly IReadOnlyDictionary<int, TextLine> targetLeadingTextLineFromSourceLine;
-        private readonly IReadOnlyDictionary<int, TextLine> targetTrailingTextLineFromSourceLine;
-        private readonly List<SyntaxTriviaList> leadingTriviaCarriedOver = new List<SyntaxTriviaList>();
-        private readonly List<SyntaxTriviaList> trailingTriviaCarriedOver = new List<SyntaxTriviaList>();
+        private readonly SyntaxNode _source;
+        private readonly TextLineCollection _sourceLines;
+        private readonly IReadOnlyDictionary<int, TextLine> _targetLeadingTextLineFromSourceLine;
+        private readonly IReadOnlyDictionary<int, TextLine> _targetTrailingTextLineFromSourceLine;
+        private readonly List<SyntaxTriviaList> _leadingTriviaCarriedOver = new List<SyntaxTriviaList>();
+        private readonly List<SyntaxTriviaList> _trailingTriviaCarriedOver = new List<SyntaxTriviaList>();
         public LineTriviaMapper(SyntaxNode source, TextLineCollection sourceLines, Dictionary<int, TextLine> targetLeadingTextLineFromSourceLine, Dictionary<int, TextLine> targetTrailingTextLineFromSourceLine)
         {
-            this.source = source;
-            this.sourceLines = sourceLines;
-            this.targetLeadingTextLineFromSourceLine = targetLeadingTextLineFromSourceLine;
-            this.targetTrailingTextLineFromSourceLine = targetTrailingTextLineFromSourceLine;
+            _source = source;
+            _sourceLines = sourceLines;
+            _targetLeadingTextLineFromSourceLine = targetLeadingTextLineFromSourceLine;
+            _targetTrailingTextLineFromSourceLine = targetTrailingTextLineFromSourceLine;
         }
 
         /// <summary>
@@ -54,7 +54,7 @@ namespace ICSharpCode.CodeConverter.Shared
             //TODO Try harder to avoid losing track of various precalculated positions changing during the replacements, for example build up a dictionary of replacements and make them in a single ReplaceTokens call
             //TODO Possible perf: Find token starting from position of last replaced token rather than from the root node each time?
 
-            for (int i = sourceLines.Count - 1; i >= 0; i--) {
+            for (int i = _sourceLines.Count - 1; i >= 0; i--) {
                 target = ConvertTrailingForSourceLine(target, i);
                 target = ConvertLeadingForSourceLine(target, i);
             }
@@ -65,19 +65,19 @@ namespace ICSharpCode.CodeConverter.Shared
 
         private SyntaxNode ConvertTrailingForSourceLine(SyntaxNode target, int sourceLineIndex)
         {
-            var sourceLine = sourceLines[sourceLineIndex];
-            var endOfSourceLine = FindNonZeroWidthToken(source, sourceLine.End);
+            var sourceLine = _sourceLines[sourceLineIndex];
+            var endOfSourceLine = FindNonZeroWidthToken(_source, sourceLine.End);
 
             if (endOfSourceLine.TrailingTrivia.Any(t => !t.IsWhitespaceOrEndOfLine())) {
-                var line = GetBestLine(targetTrailingTextLineFromSourceLine, sourceLineIndex);
+                var line = GetBestLine(_targetTrailingTextLineFromSourceLine, sourceLineIndex);
                 if (line != default) {
-                    foreach (var triviaList in trailingTriviaCarriedOver) {
+                    foreach (var triviaList in _trailingTriviaCarriedOver) {
                         target = PrependTrailingTrivia(target, line, triviaList);
                     }
-                    trailingTriviaCarriedOver.Clear();
+                    _trailingTriviaCarriedOver.Clear();
                     target = PrependTrailingTrivia(target, line, endOfSourceLine.TrailingTrivia);
                 } else if (endOfSourceLine.TrailingTrivia.Span.Start > sourceLine.Start) {
-                    trailingTriviaCarriedOver.Add(endOfSourceLine.TrailingTrivia);
+                    _trailingTriviaCarriedOver.Add(endOfSourceLine.TrailingTrivia);
                 }
             }
 
@@ -97,18 +97,18 @@ namespace ICSharpCode.CodeConverter.Shared
 
         private SyntaxNode ConvertLeadingForSourceLine(SyntaxNode target, int sourceLineIndex)
         {
-            var sourceLine = sourceLines[sourceLineIndex];
-            var startOfSourceLine = FindNonZeroWidthToken(source, sourceLine.Start);
+            var sourceLine = _sourceLines[sourceLineIndex];
+            var startOfSourceLine = FindNonZeroWidthToken(_source, sourceLine.Start);
             if (startOfSourceLine.LeadingTrivia.Any(t => !t.IsWhitespaceOrEndOfLine())) {
-                var line = GetBestLine(targetLeadingTextLineFromSourceLine, sourceLineIndex);
+                var line = GetBestLine(_targetLeadingTextLineFromSourceLine, sourceLineIndex);
                 if (line != default) {
-                    foreach (var triviaList in leadingTriviaCarriedOver) {
+                    foreach (var triviaList in _leadingTriviaCarriedOver) {
                         target = PrependLeadingTrivia(target, line, triviaList);
                     }
-                    leadingTriviaCarriedOver.Clear();
+                    _leadingTriviaCarriedOver.Clear();
                     target = PrependLeadingTrivia(target, line, startOfSourceLine.LeadingTrivia);
                 } else if (startOfSourceLine.LeadingTrivia.Span.End < sourceLine.End) {
-                    leadingTriviaCarriedOver.Add(startOfSourceLine.LeadingTrivia);
+                    _leadingTriviaCarriedOver.Add(startOfSourceLine.LeadingTrivia);
                 }
             }
 
