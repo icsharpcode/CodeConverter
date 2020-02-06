5b78e07db59803f16a998d8d888073ff1bc4cbf1
GrahamTheCoder <grahamthecoder@gmail.com>
2020-02-06 08:57:16 +0000

    Set up space for customizations

--- ICSharpCode.CodeConverter/CSharp/CommentConvertingVisitorWrapper.cs
+++ ICSharpCode.CodeConverter/CSharp/CommentConvertingVisitorWrapper.cs
@@ -4,15 +4,17 @@ using ICSharpCode.CodeConverter.Shared;
 using ICSharpCode.CodeConverter.Util;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.VisualBasic;
+using CSSyntax = Microsoft.CodeAnalysis.CSharp.Syntax;
+using VBasic = Microsoft.CodeAnalysis.VisualBasic;
+using VBSyntax = Microsoft.CodeAnalysis.VisualBasic.Syntax;
 
 namespace ICSharpCode.CodeConverter.CSharp
 {
     internal class CommentConvertingVisitorWrapper<T> where T : CSharpSyntaxNode
     {
-        private readonly VisualBasicSyntaxVisitor<Task<T>> _wrappedVisitor;
+        private readonly VBasic.VisualBasicSyntaxVisitor<Task<T>> _wrappedVisitor;
 
-        public CommentConvertingVisitorWrapper(VisualBasicSyntaxVisitor<Task<T>> wrappedVisitor)
+        public CommentConvertingVisitorWrapper(VBasic.VisualBasicSyntaxVisitor<Task<T>> wrappedVisitor)
         {
             _wrappedVisitor = wrappedVisitor;
         }
@@ -20,13 +22,22 @@ namespace ICSharpCode.CodeConverter.CSharp
         public async Task<T> Accept(SyntaxNode node, bool addSourceMapping)
         {
             try {
-                var converted = await _wrappedVisitor.Visit(node);
+                var converted = await DefaultVisit(node);
                 return addSourceMapping ? node.CopyAnnotationsTo(converted).WithSourceMappingFrom(node)
                     : converted.WithoutSourceMapping();
             } catch (Exception e) {
-                var dummyStatement = (T)(object)Microsoft.CodeAnalysis.CSharp.SyntaxFactory.EmptyStatement();
-                return dummyStatement.WithCsTrailingErrorComment((VisualBasicSyntaxNode)node, e);
+                var dummyStatement = (T)(object)SyntaxFactory.EmptyStatement();
+                return dummyStatement.WithCsTrailingErrorComment((VBasic.VisualBasicSyntaxNode)node, e);
             }
+        }
+
+        /// <remarks>
+        /// If lots of special cases, move to wrapping the wrappedVisitor in another visitor, but I'd rather use a simple switch here initially.
+        /// </remarks>
+        private async Task<T> DefaultVisit(SyntaxNode vbNode)
+        {
+            var converted = await _wrappedVisitor.Visit(vbNode);
+            return converted;
         }
     }
 }
