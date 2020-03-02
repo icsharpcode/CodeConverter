3ad34f8fc37d5daf20eb2b16e6a481461f12f7e6
GrahamTheCoder <grahamthecoder@gmail.com>
2020-03-02 09:43:41 +0000

    Add self verifying tests

---
+++ Tests/TestData/SelfVerifyingTests/CSToVB/StringEqualityTests.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualBasic.CompilerServices;
+using Xunit;
+
+public class StringEqualityTests
+{
+    private object nullObject = null;
+    private string nullString = null;
+    private object emptyStringObject = "";
+    private string emptyString = "";
+    private char[] emptyCharArray = new char[0];
+
+    [Fact]
+    public void NullEmptyStringEqualsOperator() => Assert.False(nullString == emptyString);
+
+    [Fact]
+    public void NullEmptyStringNotEqualsOperator() => Assert.True(nullString != emptyString);
+
+    [Fact]
+    public void NullEmptyObjectEqualsOperator() => Assert.False(nullObject == emptyString);
+
+    [Fact]
+    public void NullEmptyObjectNotEqualsOperator() => Assert.True(nullObject != emptyString);
+
+    [Fact]
+    public void StringCharArrayEqualsOperator() => Assert.False(emptyStringObject == emptyCharArray);
+
+    [Fact]
+    public void StringCharArrayNotEqualsOperator() => Assert.True(emptyStringObject != emptyCharArray);
+
+    [Fact]
+    public void NullObjectEqualsOperator() => Assert.True(nullObject == nullString);
+
+    [Fact]
+    public void NullObjectNotEqualsOperator() => Assert.False(nullObject != nullString);
+
+    [Fact]
+    public void VisualBasicEqualityOfNormalObjectsNotSubjectToSpecialStringConversionRules()
+    {
+        object a1 = 3;
+        object a2 = 3;
+        object b = 4;
+        Assert.False(a1 == a2, "Identical values stored in different objects should not be equal");
+        Assert.False(a1 == b, "Different values stored in different objects should not be equal");
+    }
+}
