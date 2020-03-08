8dd08fef5f2fdc828794bc38141b5d41dc6244e5
GrahamTheCoder <grahamthecoder@gmail.com>
2020-03-08 12:46:56 +0000

    Fix compile errors in embedded tree

---
+++ Tests/TestData/MultiFileCharacterization/VBToCSResults/ConvertWholeSolution/WindowsAppVb/My Project/MyNamespace.Static.3.Designer.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.VisualBasic
+{
+    [Embedded()]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Module | AttributeTargets.Assembly, Inherited = false)]
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    [System.Runtime.CompilerServices.CompilerGenerated()]
+    internal sealed class Embedded : Attribute
+    {
+    }
+}
