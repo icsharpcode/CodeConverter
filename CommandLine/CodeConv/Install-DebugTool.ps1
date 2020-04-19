8255a5bdc2ca7ec60b1353a0452a543b85f6c486
GrahamTheCoder <grahamthecoder@gmail.com>
2020-04-19 12:57:06 +0100

    Show help on debug install

--- CommandLine/CodeConv/Install-DebugTool.ps1
+++
@@ -1,8 +0,0 @@
-Param($args = $null)
-
-Push-Location "$PSScriptRoot\bin\Debug"
-
-dotnet new tool-manifest --force
-dotnet tool install ICSharpCode.CodeConverter.CodeConv --add-source .
-
-dotnet codeconv $args
