c52caca97e4390a66e6fd607c9a31de5eea5c21f
GrahamTheCoder <grahamthecoder@gmail.com>
2020-04-22 00:03:20 +0100

    Fix test input compilation

--- Tests/CSharp/ExpressionTests/ExpressionTests.cs
+++ Tests/CSharp/ExpressionTests/ExpressionTests.cs
@@ -13,7 +13,7 @@ namespace ICSharpCode.CodeConverter.Tests.CSharp.ExpressionTests
     Private Sub TestMethod()
         Dim rslt = Not 1 = 2
         Dim rslt2 = Not True
-        Dim rslt3 = TypeOf True IsNot Boolean
+        Dim rslt3 = TypeOf New Object() IsNot Boolean
     End Sub
 End Class", @"
 internal partial class TestClass
@@ -22,11 +22,9 @@ internal partial class TestClass
     {
         bool rslt = !(1 == 2);
         bool rslt2 = !true;
-        bool rslt3 = !(true is bool);
+        bool rslt3 = !(new object() is bool);
     }
-}
-1 source compilation errors:
-BC30021: 'TypeOf ... Is' requires its left operand to have a reference type, but this operand has the value type 'Boolean'.");
+}");
         }
 
         [Fact]
