4da86f1fa85f4a34233d9d638c5df7b005789b05
GrahamTheCoder <grahamthecoder@gmail.com>
2020-04-27 22:19:47 +0100

    Add test for object list

--- Tests/CSharp/ExpressionTests/ByRefTests.cs
+++ Tests/CSharp/ExpressionTests/ByRefTests.cs
@@ -469,26 +469,32 @@ public partial class Issue567
             await TestConversionVisualBasicToCSharpAsync(@"Public Class Issue567
     Sub DoSomething(ByRef str As String)
         lst = New List(Of String)({4.ToString(), 5.ToString(), 6.ToString()})
+        lst2 = New List(Of Object)({4.ToString(), 5.ToString(), 6.ToString()})
         str = 999.ToString()
     End Sub
 
     Sub Main()
         DoSomething(lst(1))
         Debug.Assert(lst(1) = 4.ToString())
+        DoSomething(lst2(1))
+        Debug.Assert(lst2(1) = 5.ToString())
     End Sub
 
 End Class
 
 Friend Module Other
     Public lst As List(Of String) = New List(Of String)({ 1.ToString(), 2.ToString(), 3.ToString()})
+    Public lst2 As List(Of Object) = New List(Of Object)({ 1.ToString(), 2.ToString(), 3.ToString()})
 End Module", @"using System.Collections.Generic;
 using System.Diagnostics;
+using Microsoft.VisualBasic.CompilerServices;
 
 public partial class Issue567
 {
     public void DoSomething(ref string str)
     {
         Other.lst = new List<string>(new[] { 4.ToString(), 5.ToString(), 6.ToString() });
+        Other.lst2 = new List<object>(new[] { 4.ToString(), 5.ToString(), 6.ToString() });
         str = 999.ToString();
     }
 
@@ -499,12 +505,18 @@ public partial class Issue567
         DoSomething(ref argstr);
         tmp[1] = argstr;
         Debug.Assert((Other.lst[1] ?? """") == (4.ToString() ?? """"));
+        var tmp1 = Other.lst2;
+        string argstr1 = Conversions.ToString(Other.lst2[1]);
+        DoSomething(ref argstr1);
+        tmp1[1] = argstr1;
+        Debug.Assert(Conversions.ToBoolean(Operators.ConditionalCompareObjectEqual(Other.lst2[1], 5.ToString(), false)));
     }
 }
 
 internal static partial class Other
 {
     public static List<string> lst = new List<string>(new[] { 1.ToString(), 2.ToString(), 3.ToString() });
+    public static List<object> lst2 = new List<object>(new[] { 1.ToString(), 2.ToString(), 3.ToString() });
 }");
         }
     }
