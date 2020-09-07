1a8b7c984e35e264d62d08810911a22234bdabfa
GrahamTheCoder <grahamthecoder@gmail.com>
2020-09-07 16:33:08 +0100

    Reformatting for consistency

--- Vsix/PasteAsCS.cs
+++ Vsix/PasteAsCS.cs
@@ -1,45 +1,47 @@
 ﻿using System;
 using System.ComponentModel.Design;
-using System.Globalization;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using ICSharpCode.CodeConverter.CSharp;
 using ICSharpCode.CodeConverter.Shared;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using IAsyncServiceProvider = Microsoft.VisualStudio.Shell.IAsyncServiceProvider;
 using Task = System.Threading.Tasks.Task;
 
 namespace ICSharpCode.CodeConverter.VsExtension
 {
     /// <summary>
-    /// Command handler
+    ///     Command handler
     /// </summary>
     internal sealed class PasteAsCS
     {
         /// <summary>
-        /// Command ID.
+        ///     Command ID.
         /// </summary>
         public const int CommandId = 0x0400;
 
         /// <summary>
-        /// Command menu group (command set GUID).
+        ///     Command menu group (command set GUID).
         /// </summary>
         public static readonly Guid CommandSet = new Guid("a3378a21-e939-40c9-9e4b-eb0cec7b7854");
 
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
-        private readonly AsyncPackage package;
-        private readonly object _codeConversion;
+        private readonly CodeConversion _codeConversion;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PasteAsCS"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
+        ///     VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PasteAsCS" /> class.
+        ///     Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
+        /// <param name="codeConversion">Instance of the code converter, not null.</param>
         /// <param name="commandService">Command service to add command to, not null.</param>
-        private PasteAsCS(CodeConverterPackage package, CodeConversion codeConversion, OleMenuCommandService commandService)
+        private PasteAsCS(CodeConverterPackage package, CodeConversion codeConversion,
+            OleMenuCommandService commandService)
         {
             this.package = package ?? throw new ArgumentNullException(nameof(package));
             _codeConversion = codeConversion;
@@ -52,81 +54,43 @@ namespace ICSharpCode.CodeConverter.VsExtension
         }
 
         /// <summary>
-        /// Gets the instance of the command.
+        ///     Gets the instance of the command.
         /// </summary>
-        public static PasteAsCS Instance {
-            get;
-            private set;
-        }
+        public static PasteAsCS Instance { get; private set; }
 
         /// <summary>
-        /// Gets the service provider from the owner package.
+        ///     Gets the service provider from the owner package.
         /// </summary>
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider {
-            get {
-                return this.package;
-            }
-        }
-        private async Task MainEditMenuItem_BeforeQueryStatusAsync(object sender, EventArgs e)
-        {
-            if (sender is OleMenuCommand menuItem) {
-                var selectionInCurrentViewAsync = await VisualStudioInteraction.GetFirstSelectedSpanInCurrentViewAsync(ServiceProvider, CodeConversion.IsCSFileName, true);
-                menuItem.Visible = selectionInCurrentViewAsync != null;
-            }
-        }
-        ///// <summary>
-        ///// Initializes the singleton instance of the command.
-        ///// </summary>
-        ///// <param name="package">Owner package, not null.</param>
-        //public static async Task InitializeAsync(AsyncPackage package)
-        //{
-        //    // Switch to the main thread - the call to AddCommand in PasteAsCS's constructor requires
-        //    // the UI thread.
-        //    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+        private IAsyncServiceProvider ServiceProvider => package;
 
-        //    OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-        //    Instance = new PasteAsCS(package, commandService);
-        //}
         /// <remarks>
-        /// Must be called from UI thread
+        ///     Must be called from UI thread
         /// </remarks>
-        public static void Initialize(CodeConverterPackage package, OleMenuCommandService menuCommandService, CodeConversion codeConversion)
+        public static void Initialize(CodeConverterPackage package, OleMenuCommandService menuCommandService,
+            CodeConversion codeConversion)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
-            //Instance = new ConvertVBToCSCommand(package, codeConversion, menuCommandService);
             Instance = new PasteAsCS(package, codeConversion, menuCommandService);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
-        private void Execute(object sender, EventArgs e)
+        private async Task MainEditMenuItem_BeforeQueryStatusAsync(object sender, EventArgs e)
         {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            string message = "Here is the code \n" + Clipboard.GetText(); //string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "PasteAsCS";
-
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            if (sender is OleMenuCommand menuItem) {
+                var selectionInCurrentViewAsync =
+                    await VisualStudioInteraction.GetFirstSelectedSpanInCurrentViewAsync(ServiceProvider,
+                        CodeConversion.IsCSFileName, true);
+                menuItem.Visible = selectionInCurrentViewAsync != null;
+            }
         }
 
         private async Task CodeEditorMenuItemCallbackAsync(CancellationToken cancellationToken)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            var text = Clipboard.GetText();
-            var convertTextOnly = await ProjectConversion.ConvertTextAsync<VBToCSConversion>(text, conversionOptions: new TextConversionOptions(DefaultReferences.NetStandard2), cancellationToken: cancellationToken);
-            await VisualStudioInteraction.WriteToCurrentWindowAsync(ServiceProvider,convertTextOnly.ConvertedCode);
+            string text = Clipboard.GetText();
+            var convertTextOnly = await ProjectConversion.ConvertTextAsync<VBToCSConversion>(text,
+                new TextConversionOptions(DefaultReferences.NetStandard2),
+                cancellationToken: cancellationToken);
+            await VisualStudioInteraction.WriteToCurrentWindowAsync(ServiceProvider, convertTextOnly.ConvertedCode);
         }
-
     }
 }
