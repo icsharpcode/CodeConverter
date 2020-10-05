4ce79efb9e056dca61a01da70ba3955f30a6d933
GrahamTheCoder <grahamthecoder@gmail.com>
2020-10-06 00:21:06 +0100

    Turn null into undefined to please typescript

--- Web/ClientApp/src/index.tsx
+++ Web/ClientApp/src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') || undefined;
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
