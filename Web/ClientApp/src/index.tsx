58f6663d0fd3db774a20d84265594f44e26af0cc
Adam Stachowicz <saibamenppl@gmail.com>
2020-10-07 00:59:37 +0200

    Update index.tsx

--- Web/ClientApp/src/index.tsx
+++ Web/ClientApp/src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') || undefined;
 const rootElement = document.getElementById('root');
@@ -14,4 +14,7 @@ ReactDOM.render(
     </BrowserRouter>,
     rootElement);
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
