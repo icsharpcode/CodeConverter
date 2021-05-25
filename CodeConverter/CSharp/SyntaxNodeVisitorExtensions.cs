197160058fe30aad46eeb09181bebbf97473af12
GrahamTheCoder <GrahamTheCoder@gmail.com>
2021-05-25 11:45:23 +0100

    Expression body and doc comment

--- CodeConverter/CSharp/SyntaxNodeVisitorExtensions.cs
+++ CodeConverter/CSharp/SyntaxNodeVisitorExtensions.cs
@@ -8,12 +8,9 @@ namespace ICSharpCode.CodeConverter.CSharp
     [System.Diagnostics.DebuggerStepThrough]
     internal static class SyntaxNodeVisitorExtensions
     {
-        public static async Task<TOut> AcceptAsync<TOut>(this SyntaxNode node, CommentConvertingVisitorWrapper visitorWrapper, SourceTriviaMapKind sourceTriviaMap = SourceTriviaMapKind.All) where TOut : CSharpSyntaxNode
-        {
-            if (node == null) return null;
-            return await visitorWrapper.AcceptAsync<TOut>(node, sourceTriviaMap);
-        }
-
+        /// <summary>For TOut, specify the most general type acceptable by the calling code (often ExpressionSyntax), this allows error information to be attached to a dummy return node.</summary>
+        public static async Task<TOut> AcceptAsync<TOut>(this SyntaxNode node, CommentConvertingVisitorWrapper visitorWrapper, SourceTriviaMapKind sourceTriviaMap = SourceTriviaMapKind.All) where TOut : CSharpSyntaxNode =>
+            node == null ? null : await visitorWrapper.AcceptAsync<TOut>(node, sourceTriviaMap);
         public static async Task<SeparatedSyntaxList<TOut>> AcceptSeparatedListAsync<TIn, TOut>(this SeparatedSyntaxList<TIn> nodes, CommentConvertingVisitorWrapper visitorWrapper, SourceTriviaMapKind sourceTriviaMap = SourceTriviaMapKind.All) where TIn : VisualBasicSyntaxNode where TOut : CSharpSyntaxNode =>
             await visitorWrapper.AcceptAsync<TIn, TOut>(nodes, sourceTriviaMap);
     }
