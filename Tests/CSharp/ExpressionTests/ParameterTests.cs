a341cedb0761d839b70c37d748c816f312cbc16b
Sampo Kivistö <sampo.kivisto@visma.com>
2021-09-12 12:34:34 +0300

    Fixes an issue where Visual Basic optional parameter fails conversion to C#

---
+++ Tests/CSharp/ExpressionTests/ParameterTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using ICSharpCode.CodeConverter.Tests.TestRunners;
+using Xunit;
+
+namespace ICSharpCode.CodeConverter.Tests.CSharp.ExpressionTests
+{
+    public class ParameterTests : ConverterTestBase
+    {
+        [Fact]
+        public async Task OptionalParameter_DoesNotThrowInvalidCastException()
+        {
+            await TestConversionVisualBasicToCSharpAsync(@"
+Public Class MyAttribute
+    Inherits Attribute
+End Class
+
+Public Class MyController
+    Public Function GetNothing(
+        <MyAttribute()> Optional indexer As Integer? = 0
+    ) As String
+        Return Nothing
+    End Function
+End Class
+",
+@"using System;
+
+public partial class MyAttribute : Attribute
+{
+}
+
+public partial class MyController
+{
+    public string GetNothing([My()] int? indexer = 0)
+    {
+        return null;
+    }
+}");
+        }
+    }
+}
