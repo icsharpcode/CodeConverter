7d579727dfc7ff81f0710c9ae6cd043b4646582d
Bing Cheng <bingcheng21@outlook.com>
2021-09-17 18:19:50 +0000

    Add a console application

---
+++ CommandLine/CodeConvConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using ICSharpCode.CodeConverter.CommandLine;
+
+namespace CodeConvConsoleApp
+{
+    class Program
+    {
+
+
+        static async Task MainAsync(string[] args)
+        {
+            var result = await CodeConvProgram.Main(args);
+
+            Console.WriteLine($"Done with return value: {result}");
+
+            return;
+        }
+    }
+
+}
+
+
+namespace ICSharpCode.CodeConverter.CommandLine
+{
+
+    public  partial class CodeConvProgram
+    {
+        private static readonly bool _runningInNetCore = true;
+    }
+}
