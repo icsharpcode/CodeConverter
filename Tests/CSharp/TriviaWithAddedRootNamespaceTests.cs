4e3099649cc5102ac747833e23d9c00a39fe3610
GrahamTheCoder <GrahamTheCoder@gmail.com>
2021-10-09 17:42:51 +0100

    New failing test

---
+++ Tests/CSharp/TriviaWithAddedRootNamespaceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using ICSharpCode.CodeConverter.Tests.TestRunners;
+using Xunit;
+
+namespace ICSharpCode.CodeConverter.Tests.CSharp
+{
+    public class TriviaWithAddedRootNamespaceTests : ConverterTestBase
+    {
+        public TriviaWithAddedRootNamespaceTests() : base("ANamespace")
+        {
+
+        }
+
+        [Fact]
+        public async Task CommentAtStartOfFile663Async()
+        {
+            await TestConversionVisualBasicToCSharpAsync(
+@"''' <summary> Form for viewing the my. </summary>
+''' <remarks> David, 10/1/2020. </remarks>
+Public Class MyForm
+    Inherits isr.Automata.Finite.Forms.BimanualToggleForm
+End Class",
+@"
+public partial class AClass
+{
+    /* TODO ERROR:sdAMethod()
+    {
+    }
+    /* TODO ERROR: Skipped EndIfDirectiveTrivia
+#End If
+*/
+}
+");
+        }
+    }
+}
