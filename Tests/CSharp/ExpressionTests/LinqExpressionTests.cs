e132420905773d753906bad44db3f94092445896
GrahamTheCoder <GrahamTheCoder@gmail.com>
2022-03-27 23:45:30 +0100

    Make test cover Issue853

--- Tests/CSharp/ExpressionTests/LinqExpressionTests.cs
+++ Tests/CSharp/ExpressionTests/LinqExpressionTests.cs
@@ -711,7 +711,7 @@ Public Class VisualBasicClass
 	    Dim list1 As New List(Of Integer)() From {1,2,3}
 	    Dim list2 As New List(Of Integer) From {2, 4,5}
 	
-	    Dim qs = From n In list1 from x In list2
+	    Dim qs = From n In list1, x In list2
 			     Where x = n 
 			     Select New With {x, n}
     End Sub
