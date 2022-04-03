4a353671853ab6badcf2cc4bdd420a5c7839ba1e
GrahamTheCoder <GrahamTheCoder@gmail.com>
2022-04-03 11:40:19 +0100

    Add global usings for aliases used all over project

---
+++ CodeConverter/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using VBasic = Microsoft.CodeAnalysis.VisualBasic;
+global using CS = Microsoft.CodeAnalysis.CSharp;
+global using VBSyntax = Microsoft.CodeAnalysis.VisualBasic.Syntax;
+global using CSSyntax = Microsoft.CodeAnalysis.CSharp.Syntax;
+
