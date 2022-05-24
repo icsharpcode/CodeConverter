a0746a4f13803ce363a4a59a3c7301924d46f256
Dominik Baran <dominik.baran7@gmail.com>
2022-05-24 23:21:09 +0200

    Add test for xmlns builtin namespace

--- Tests/CSharp/ExpressionTests/XmlExpressionTests.cs
+++ Tests/CSharp/ExpressionTests/XmlExpressionTests.cs
@@ -324,6 +324,7 @@ internal static partial class Module1
         await TestConversionVisualBasicToCSharpAsync(@"Class TestClass
     Private Sub TestMethod()       
         Dim xElement = <Name xml:lang=""de"">Beispiel</Name>
+        Dim xElement2 = <Name xmlns:ns1=""http://www.example.com/namespace/1"">Beispiel</Name>
     End Sub
 End Class", @"using System.Xml.Linq;
 
@@ -332,6 +333,7 @@ internal partial class TestClass
     private void TestMethod()
     {
         var xElement = new XElement(""Name"", new XAttribute(XNamespace.Xml + ""lang"", ""de""), ""Beispiel"");
+        var xElement2 = new XElement(""Name"", new XAttribute(XNamespace.Xmlns + ""ns1"", ""http://www.example.com/namespace/1""), ""Beispiel"");
     }
 }");
 
