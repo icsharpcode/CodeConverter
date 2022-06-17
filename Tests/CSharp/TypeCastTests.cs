78e7d489d7894b4cbfee5d610a14d3e29ee22bcd
Dominik Baran <dominik.baran7@gmail.com>
2022-06-17 14:22:47 +0200

    Fix casting object to nullable types

--- Tests/CSharp/TypeCastTests.cs
+++ Tests/CSharp/TypeCastTests.cs
@@ -1289,6 +1289,40 @@ End Sub
 ");
     }
 
+    
+    [Fact]
+    public async Task CastObjectToNullableAsync()
+    {
+        await TestConversionVisualBasicToCSharpAsync(
+            @"Class CastTest
+    Private Function Test(input as Object) As Integer?
+            Return CType(input, Integer?)
+    End Function
+    Private Function Test2(input as Object) As Decimal?
+        Return CType(input, Nullable(Of Double))
+    End Function
+    Private Function Test2(input as Integer) As Decimal?
+        Return CType(input, Nullable(Of Double))
+    End Function
+End Class", @"
+internal partial class CastTest
+{
+    private int? Test(object input)
+    {
+        return (int?)input;
+    }
+    private decimal? Test2(object input)
+    {
+        return (decimal?)input;
+    }
+    private decimal? Test2(int input)
+    {
+        return input;
+    }
+}");
+    }
+
+
     [Fact]
     public async Task TestSingleCharacterStringLiteralBecomesCharWhenNeededAsync()
     {
