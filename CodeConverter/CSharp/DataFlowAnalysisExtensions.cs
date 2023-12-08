b0a64e65d6c53b525a65b46ce9fce084b5ce5da7
Timur Kelman <Timur.Kelman@fecher.eu>
2023-12-08 12:06:01 +0100

    add a workaround
    It's only required until https://github.com/dotnet/roslyn/issues/71115 is fixed.

---
+++ CodeConverter/CSharp/DataFlowAnalysisExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICSharpCode.CodeConverter.CSharp;
+internal static class DataFlowAnalysisExtensions
+{
+    /// <summary>
+    /// Accesses the <see cref="DataFlowAnalysis.ReadInside" /> second time in case of exception.
+    /// This is a workaround for a bug present in Roslyn up to version 4.8.0
+    /// (https://github.com/dotnet/roslyn/issues/71115)
+    /// </summary>
+    public static System.Collections.Immutable.ImmutableArray<ISymbol> ReadInsideSafe(this DataFlowAnalysis dataFlow)
+    {
+        try
+        {
+            return dataFlow.ReadInside;
+        }
+        catch
+        {
+            return dataFlow.ReadInside;
+        }
+    }
+}
--- CodeConverter/CSharp/DefiniteAssignmentAnalyzer.cs
+++ CodeConverter/CSharp/DefiniteAssignmentAnalyzer.cs
@@ -5,7 +5,7 @@ internal static class DefiniteAssignmentAnalyzer
 
     public static bool IsDefinitelyAssignedBeforeRead(ISymbol localSymbol, DataFlowAnalysis methodFlow)
     {
-        if (!methodFlow.ReadInside.Contains(localSymbol)) return true;
+        if (!methodFlow.ReadInsideSafe().Contains(localSymbol)) return true;
         var unassignedVariables = methodFlow.GetVbUnassignedVariables();
         return unassignedVariables != null && !unassignedVariables.Contains(localSymbol, SymbolEqualityComparer.IncludeNullability);
     }
