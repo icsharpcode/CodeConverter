ccdcbf9b39cd0ecb357c5aaaa35ef45c65c3c172
Georgi Yankov <gyankov@progress.com>
2024-05-28 15:57:50 +0300

    chore: add security headers

--- Web/Program.cs
+++ Web/Program.cs
@@ -2,21 +2,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHsts(h => {
+    h.MaxAge = TimeSpan.FromDays(365);
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.TryAdd("X-Content-Type-Options", "nosniff");
+        await next();
+    });
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
