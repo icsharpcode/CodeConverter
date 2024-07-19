4575a358ed652c50d22bc6a98c240b512e1eeff2
Timur Kelman <Timur.Kelman@fecher.eu>
2024-07-19 12:14:27 +0200

    adjust other tests

--- Tests/TestData/MultiFileCharacterization/CSToVBResults/ConvertWholeSolution/CSharpRefReturn/RefReturnList.vb
+++ Tests/TestData/MultiFileCharacterization/CSToVBResults/ConvertWholeSolution/CSharpRefReturn/RefReturnList.vb
@@ -3,15 +3,33 @@ Namespace CSharpRefReturn
     Public Class RefReturnList(Of T)
         Private dummy As T
                 ''' Cannot convert IndexerDeclarationSyntax, System.InvalidCastException: Unable to cast object of type 'Microsoft.CodeAnalysis.VisualBasic.Syntax.EmptyStatementSyntax' to type 'Microsoft.CodeAnalysis.VisualBasic.Syntax.TypeSyntax'.
-'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.<>c__DisplayClass53_0.<ConvertPropertyBlock>b__1()
-'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.ConvertPropertyBlock(BasePropertyDeclarationSyntax node, SyntaxToken id, SyntaxTokenList modifiers, ParameterListSyntax parameterListSyntax, ArrowExpressionClauseSyntax arrowExpressionClauseSyntax, EqualsValueSyntax initializerOrNull)
-'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitIndexerDeclaration(IndexerDeclarationSyntax node)
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.<>c__DisplayClass53_0.<ConvertPropertyBlock>b__1() in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\NodesVisitor.cs:line 543
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.ConvertPropertyBlock(BasePropertyDeclarationSyntax node, SyntaxToken id, SyntaxTokenList modifiers, ParameterListSyntax parameterListSyntax, ArrowExpressionClauseSyntax arrowExpressionClauseSyntax, EqualsValueSyntax initializerOrNull) in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\NodesVisitor.cs:line 576
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitIndexerDeclaration(IndexerDeclarationSyntax node) in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\NodesVisitor.cs:line 526
 '''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
-'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping) in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\CommentConvertingVisitorWrapper.cs:line 20
 ''' 
 ''' Input:
 '''         public ref T this[int i] {
 '''             get {
+'''                 return ref this.dummy;
+'''             }
+'''         }
+''' 
+''' 
+                ''' Cannot convert PropertyDeclarationSyntax, System.InvalidCastException: Unable to cast object of type 'Microsoft.CodeAnalysis.VisualBasic.Syntax.EmptyStatementSyntax' to type 'Microsoft.CodeAnalysis.VisualBasic.Syntax.TypeSyntax'.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.<>c__DisplayClass53_0.<ConvertPropertyBlock>b__1() in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\NodesVisitor.cs:line 543
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.ConvertPropertyBlock(BasePropertyDeclarationSyntax node, SyntaxToken id, SyntaxTokenList modifiers, ParameterListSyntax parameterListSyntax, ArrowExpressionClauseSyntax arrowExpressionClauseSyntax, EqualsValueSyntax initializerOrNull) in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\NodesVisitor.cs:line 576
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitPropertyDeclaration(PropertyDeclarationSyntax node) in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\NodesVisitor.cs:line 514
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping) in C:\_Work\thirdPartyTools\CodeConverter\CodeConverter\VB\CommentConvertingVisitorWrapper.cs:line 20
+''' 
+''' Input:
+''' 
+'''         public ref T RefProperty
+'''         {
+'''             get
+'''             {
 '''                 return ref this.dummy;
 '''             }
 '''         }
--- Tests/TestData/MultiFileCharacterization/VBToCSResults/ConvertWholeSolution/VisualBasicUsesCSharpRefReturn/ByRefArgument.cs
+++ Tests/TestData/MultiFileCharacterization/VBToCSResults/ConvertWholeSolution/VisualBasicUsesCSharpRefReturn/ByRefArgument.cs
@@ -1,18 +1,35 @@
-﻿
+﻿using Microsoft.VisualBasic.CompilerServices;
+
 namespace VisualBasicUsesCSharpRefReturn
 {
     public class ByRefArgument
     {
         public void UseArr()
         {
-            var arr = default(object[]);
-            Modify(ref arr[0]);
+            var arrObj = default(object[]);
+            Modify(ref arrObj[0]);
+
+            var arrInt = default(int[]);
+            var tmp = arrInt;
+            object argo = tmp[0];
+            Modify(ref argo);
+            tmp[0] = Conversions.ToInteger(argo);
         }
 
         public void UseRefReturn()
         {
-            var lst = default(CSharpRefReturn.RefReturnList<object>);
-            Modify(ref lst[0]);
+            var lstObj = default(CSharpRefReturn.RefReturnList<object>);
+            Modify(ref lstObj[0]);
+            Modify(ref lstObj.RefProperty);
+
+            var lstInt = default(CSharpRefReturn.RefReturnList<int>);
+            var tmp = lstInt;
+            object argo = tmp[0];
+            Modify(ref argo);
+            tmp[0] = Conversions.ToInteger(argo);
+            object argo1 = lstInt.RefProperty;
+            Modify(ref argo1);
+            lstInt.RefProperty = Conversions.ToInteger(argo1);
         }
 
         public void Modify(ref object o)
