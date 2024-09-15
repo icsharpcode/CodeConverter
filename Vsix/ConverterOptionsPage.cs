5894bb08cde2b7ed43bafd5d4118cbfb8e0a17ba
GrahamTheCoder <GrahamTheCoder@gmail.com>
2024-09-15 17:33:00 +0100

    Convert options page based on (gpt-4o). Removed final option since it shouldn't matter now hopefully...
    Prompt:
    I'm converting a Visual Studio extension to use the out-of-process visual studio extensibility framework
    
    Give the full code converting these settings into the new style
    <current>
    
    Here's a sample project in the extensibility framework as an example:
    < class from https://github.com/microsoft/VSExtensibility/blob/main/New_Extensibility_Model/Samples/SettingsSample/SettingDefinitions.cs >

--- Vsix/ConverterOptionsPage.cs
+++ Vsix/ConverterOptionsPage.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
-using CodeConv.Shared.Util;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.Extensibility.Settings;
+using Microsoft.VisualStudio.Extensibility;
 using Microsoft.VisualStudio.Shell;
 
 namespace ICSharpCode.CodeConverter.VsExtension;
@@ -8,41 +10,32 @@ namespace ICSharpCode.CodeConverter.VsExtension;
 /// <summary>
 /// Sample https://github.com/microsoft/VSExtensibility/blob/main/New_Extensibility_Model/Samples/SettingsSample/SettingDefinitions.cs
 /// </summary>
-internal sealed class ConverterOptionsPage : DialogPage
+[Experimental("VSEXTPREVIEW_SETTINGS")]
+internal static class ConverterSettings
 {
-    private const string SettingsPageCategory = "Settings";
-    private bool _bypassAssemblyLoadingErrors;
+    [VisualStudioContribution]
+    internal static SettingCategory ConverterSettingsCategory { get; } = new("converterSettings", "Settings") {
+        Description = "Settings for the Converter extension.",
+    };
 
-    [Category(SettingsPageCategory)]
-    [DisplayName("Copy result to clipboard")]
-    [Description("When a single document conversion finishs, copy the the result (if any) to the clipboard.")]
-    public bool CopyResultToClipboardForSingleDocument { get; set; }
+    [VisualStudioContribution]
+    internal static Setting.Boolean CopyResultToClipboardForSingleDocument { get; } = new("copyResultToClipboardForSingleDocument", "Copy result to clipboard", ConverterSettingsCategory, defaultValue: false) {
+        Description = "When a single document conversion finishes, copy the result (if any) to the clipboard.",
+    };
 
-    [Category(SettingsPageCategory)]
-    [DisplayName("Overwrite files without warning")]
-    [Description("When a project is converted, the solution and any referencing project files need to be updated. Setting this option to true skips the dialog box usually displayed.")]
-    public bool AlwaysOverwriteFiles { get; set; }
+    [VisualStudioContribution]
+    internal static Setting.Boolean AlwaysOverwriteFiles { get; } = new("alwaysOverwriteFiles", "Overwrite files without warning", ConverterSettingsCategory, defaultValue: false) {
+        Description = "When a project is converted, the solution and any referencing project files need to be updated. Setting this option to true skips the dialog box usually displayed.",
+    };
 
-    [Category(SettingsPageCategory)]
-    [DisplayName("Create backups")]
-    [Description("When a project is converted, the solution and any referencing project files need to be updated. Setting this option to false skips creating '.bak' files for overwritten files.")]
-    public bool CreateBackups { get; set; } = true;
+    [VisualStudioContribution]
+    internal static Setting.Boolean CreateBackups { get; } = new("createBackups", "Create backups", ConverterSettingsCategory, defaultValue: true) {
+        Description = "When a project is converted, the solution and any referencing project files need to be updated. Setting this option to false skips creating '.bak' files for overwritten files.",
+    };
 
-    [Category(SettingsPageCategory)]
-    [DisplayName("Comment and formatting timeout (minutes)")]
-    [Description("Roslyn formatting can take a very long time for large files and has no progress updates. Set this to the maximum you're willing to wait without any indication of progress.")]
-    public int FormattingTimeout { get; set; } = 15;
-
-    [Category(SettingsPageCategory)]
-    [DisplayName("Bypass assembly loading errors")]
-    [Description("Attempts to workaround 'Could not load file or assembly' errors by loading any assembly version available.")]
-    public bool BypassAssemblyLoadingErrors {
-        get {
-            return _bypassAssemblyLoadingErrors;
-        }
-        set {
-            _bypassAssemblyLoadingErrors = value;
-            AppDomain.CurrentDomain.UseVersionAgnosticAssemblyResolution(value);
-        }
-    }
+    [VisualStudioContribution]
+    internal static Setting.Integer FormattingTimeout { get; } = new("formattingTimeout", "Comment and formatting timeout (minutes)", ConverterSettingsCategory, defaultValue: 15) {
+        Description = "Roslyn formatting can take a very long time for large files and has no progress updates. Set this to the maximum you're willing to wait without any indication of progress.",
+        Minimum = 1,
+    };
 }
