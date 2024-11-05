51b0ff1446d77aef28d5eaaf88c5f6e684275b31
gaschd <gaschd@users.noreply.github.com>
2024-11-06 00:47:48 +0100

    AddressOf delegate signatures can accept interfaces as compatible return types

--- CodeConverter/Util/FromRoslyn/IMethodSymbolExtensions.cs
+++ CodeConverter/Util/FromRoslyn/IMethodSymbolExtensions.cs
@@ -23,7 +23,7 @@ internal static class IMethodSymbolExtensions
             return false;
         }
 
-        if (!method.ReturnType.InheritsFromOrEquals(invoke.ReturnType)) {
+        if (!method.ReturnType.InheritsFromOrEquals(invoke.ReturnType, true)) {
             return false;
         }
 
