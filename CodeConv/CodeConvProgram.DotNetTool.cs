8c2b954f1f345e5b191e5b07509ab7e5260b46fe
Graham <GrahamTheCoder@gmail.com>
2026-02-06 00:24:42 +0000

    Remove unused

--- CodeConv/CodeConvProgram.DotNetTool.cs
+++
@@ -1,6 +0,0 @@
-namespace ICSharpCode.CodeConverter.CommandLine;
-
-public partial class CodeConvProgram
-{
-    private static readonly bool _runningInNetCore = true;
-}
--- CodeConv/CodeConvProgram.cs
+++ CodeConv/CodeConvProgram.cs
@@ -56,9 +56,6 @@ public partial class CodeConvProgram
     [Option("-f|--force", "Wipe the output directory before conversion", CommandOptionType.NoValue)]
     public bool Force { get; }
 
-    [Option(CoreOptionDefinition, "Force dot net core build if converting only .NET Core projects and seeing pre-conversion compile errors", CommandOptionType.NoValue)]
-    public bool CoreOnlyProjects { get; }
-
     [Option("-b|--best-effort", "Overrides warnings about compilation issues with input, and attempts a best effort conversion anyway", CommandOptionType.NoValue)]
     public bool BestEffort { get; }
 
@@ -74,12 +71,6 @@ public partial class CodeConvProgram
 
     private async Task<int> ExecuteAsync()
     {
-        // Ideally we'd be able to use MSBuildLocator.QueryVisualStudioInstances(DiscoveryType.VisualStudioSetup) from .NET core, but it will never be supported: https://github.com/microsoft/MSBuildLocator/issues/61
-        // Instead, if MSBuild 16.0+ is available, start a .NET framework process and let it run with that
-        if (_runningInNetCore && !CoreOnlyProjects) {
-            await Console.Error.WriteLineAsync($"Net Framework projects detected. This is not supported after codeconv 9.2.7. Run an older version from  github.com/icsharpcode/CodeConverter");
-        }
-
         try {
             Progress<ConversionProgress> progress = new Progress<ConversionProgress>(s => Console.Out.WriteLine(s.ToString()));
             await ConvertAsync(progress, CancellationToken.None);
@@ -104,23 +95,6 @@ public partial class CodeConvProgram
         Console.WriteLine();
         Console.WriteLine("Exiting successfully. Report any issues at github.com/icsharpcode/CodeConverter to help us improve the accuracy of future conversions");
         return 0;
-    }
-
-    private static async Task<int> RunNetFrameworkExeAsync(string latestMsBuildExePath)
-    {
-        Console.WriteLine($"Using .NET Framework MSBuild from {latestMsBuildExePath}");
-        var assemblyDirectoryPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
-        var args = Environment.GetCommandLineArgs().Skip(1).ToArray();
-        if (string.IsNullOrWhiteSpace(assemblyDirectoryPath)) throw new InvalidOperationException("Could not retrieve executing assembly directory");
-        var netFrameworkExe = Path.Combine(assemblyDirectoryPath, "..", "..", "NetFramework", "ICSharpCode.CodeConverter.CodeConv.NetFramework.exe");
-        if (!File.Exists(netFrameworkExe) && Path.GetDirectoryName(assemblyDirectoryPath) is { } assemblyDirectoryParentPath) {
-
-            var debugAssemblyDirectoryPath = assemblyDirectoryParentPath.Replace("CommandLine\\CodeConv\\", "CommandLine\\CodeConv.NetFramework\\");
-            var debugNetFrameworkExe = Path.Combine(debugAssemblyDirectoryPath, "ICSharpCode.CodeConverter.CodeConv.NetFramework.exe");
-            netFrameworkExe = File.Exists(debugNetFrameworkExe) ? debugNetFrameworkExe : throw new FileNotFoundException($"Cannot find net framework exe at `{netFrameworkExe}`. Using the --core-only flag to get work around this.");
-        }
-        var exitCode = await ProcessRunner.ConnectConsoleGetExitCodeAsync(netFrameworkExe, args);
-        return exitCode;
     }
 
     private async Task ConvertAsync(IProgress<ConversionProgress> progress, CancellationToken cancellationToken)
@@ -174,22 +148,5 @@ public partial class CodeConvProgram
         string projectFilePath = project.FilePath ?? "";
         var isIncluded = !Include.Any() || Include.Any(regex => Regex.IsMatch(projectFilePath, regex));
         return isIncluded && Exclude.All(regex => !Regex.IsMatch(projectFilePath, regex));
-    }
-
-    private static async Task<string?> GetLatestMsBuildExePathAsync()
-    {
-        // The second path here is available on github action agents: https://github.com/microsoft/setup-msbuild#how-does-this-work
-        var pathsToCheck = new[] {@"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe", @"%ProgramData%\chocolatey\bin"}
-            .Select(Environment.ExpandEnvironmentVariables).ToArray();
-        var vsWhereExe = pathsToCheck.FirstOrDefault(File.Exists)
-                         ?? throw new FileNotFoundException($"Could not find VSWhere in: {string.Join(", ", pathsToCheck.Select(p => $"`{p}`"))}");
-        var args = new[] { "-latest", "-prerelease", "-products", "*", "-requires", "Microsoft.Component.MSBuild", "-version", "[16.0,]", "-find", @"MSBuild\**\Bin\MSBuild.exe" };
-            
-        var (exitCode, stdOut, _) = await new ProcessStartInfo(vsWhereExe) {
-            Arguments = ArgumentEscaper.EscapeAndConcatenate(args)
-        }.GetOutputAsync();
-
-        if (exitCode == 0 && !string.IsNullOrWhiteSpace(stdOut)) return stdOut;
-        return null;
     }
 }
