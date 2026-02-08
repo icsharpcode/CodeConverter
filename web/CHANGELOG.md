e5c8f9782ae5b52d7f133b98b2af58b6c44b3a4b
Graham <GrahamTheCoder@gmail.com>
2026-02-08 22:57:46 +0000

    Move existing code in

---
+++ web/CHANGELOG.md
@@ -0,0 +1,11 @@
+This file explains how Visual Studio created the project.
+
+The following tools were used to generate this project:
+- create-vite
+
+The following steps were used to generate this project:
+- Create react project with create-vite: `npm init --yes vite@latest web -- --template=react-ts`.
+- Create project file (`web.esproj`).
+- Create `launch.json` to enable debugging.
+- Add project to solution.
+- Write this file.
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as Api from "./Api"
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./custom.css"
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+const App = () => {
+    const [versionString, setVersionString] = useState("");
+    useEffect(() => {
+        Api.getVersion()
+            .then((response: any) => {
+                setVersionString(response.data);
+            });
+    }, []);
+
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+            <hr />
+            <footer>
+                <p>	&copy; 2015-{new Date().getFullYear()} - Code Converter {versionString && (<span>{versionString} </span>)}by <a href="https://github.com/icsharpcode/CodeConverter/">https://github.com/icsharpcode/CodeConverter/</a></p>
+            </footer>
+        </Layout>
+    );
+};
+
+export default App;
