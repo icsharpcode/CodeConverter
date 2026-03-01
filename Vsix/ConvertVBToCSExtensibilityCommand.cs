3f26986a4619ce27824556c411796d64c779c9cb
Graham <GrahamTheCoder@gmail.com>
2026-03-01 17:38:19 +0000

    Phase 2: Add initial Extensibility SDK command contributions

---
+++ Vsix/ConvertVBToCSExtensibilityCommand.cs
@@ -0,0 +1,25 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+
+namespace ICSharpCode.CodeConverter.VsExtension;
+
+[VisualStudioContribution]
+public class ConvertVBToCSExtensibilityCommand : Command
+{
+    private readonly CodeConverterPackage _package;
+
+    public ConvertVBToCSExtensibilityCommand(CodeConverterPackage package)
+    {
+        _package = package;
+    }
+
+    public override CommandConfiguration CommandConfiguration => new("%a3378a21-e939-40c9-9e4b-eb0cec7b7854%");
+
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+    {
+        // This will be wired up to the existing logic in Phase 2
+        await Task.CompletedTask;
+    }
+}
